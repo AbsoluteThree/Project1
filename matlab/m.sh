e8ed0394d5d737f3bbccbe0c81b13b556420627c
AbsoluteThree <18586750391@163.com>
2024-02-11 21:04:28 +0800

    d

--- matlab/m.sh
+++ matlab/m.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 /Applications/Polyspace/R2020b.app/bin/matlab -nosplash -nodesktop
+#  1.添加git全局范围的用户名和邮箱：
 #  git config --global user.email "18586750391@163.com"
 #  git config --global user.name "AbsoluteThree"
+#  2.配置完确定下查看：
+#  看全局：
+#  git config --global -l
+#  看所有的：
 #  git config -l
+#  3.删除全局git
+#  git config --global --unset user.name
+#  git config --global --unset user.email
+
