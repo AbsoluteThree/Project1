3c3ee40c5fcd27a3d547bbdf533c706a7962b51f
AbsoluteThree <18586750391@163.com>
2024-02-11 20:56:30 +0800

    s

--- matlab/m.sh
+++ matlab/m.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 /Applications/Polyspace/R2020b.app/bin/matlab -nosplash -nodesktop
+#  git config --global user.email "18586750391@163.com"
+#  git config --global user.name "AbsoluteThree"
+#  git config -l
